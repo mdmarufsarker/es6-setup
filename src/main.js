66a3e3336af241a1b5b235a1e0b0c2432b28eeef
mdmarufsarker <mdmarufsarker2021@gmail.com>
2022-12-11 04:09:45 +0600

    webpack setup with scss

--- src/main.js
+++
@@ -1,9 +0,0 @@
-let test = "Hello World"
-
-const myFunc = (name, age) => {
-	return `Hi ${name}, Your age is ${age} year old!`;
-}
-
-console.log(myFunc('Maruf', 18))
-
-console.log("Thanks for using me :D")
